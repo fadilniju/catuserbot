d5234b974a486d84b5046d49f43f024593d76e6f
fadilniju <76585854+fadilniju@users.noreply.github.com>
2021-08-10 18:55:07 +0530

    Update secret.py

--- userbot/assistant/secret.py
+++ userbot/assistant/secret.py
@@ -20,7 +20,7 @@ async def on_plug_in_callback_query_handler(event):
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
             else:
-                reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
+                reply_pop_up_alert = "എന്താ myre നിനക്ക് ഇതിനുള്ളിൽ കാര്യം, "
         except KeyError:
             reply_pop_up_alert = "This message no longer exists in catub server"
     else:
